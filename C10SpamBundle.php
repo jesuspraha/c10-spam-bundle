e88f2ea2d6ffa421096a1f76b883f73d7be6d070
jesuspraha <jesus@orechovka.cz>
2025-07-07 11:05:47 +0200

    mistake in file placement

--- C10SpamBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace C10\SpamBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class C10SpamBundle extends Bundle
-{
-}
---
+++ src/C10SpamBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace C10\SpamBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class C10SpamBundle extends Bundle
+{
+}
